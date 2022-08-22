a784fe2b6ae7b5464fce47468ae4045efc1c7d7a
Jonatan Murcia <jonatanjosmurcia@gmail.com>
2022-08-22 11:45:29 -0500

    Winston logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,45 @@
-const logger = (msg, type = "log")=>{
+const winston = require('winston');
+
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6
+  };
+
+
+  const logFormatter = winston.format.printf((info) => {
+    let { stack, message } = info;
+    message = stack || message;
+    return  message;
+  });
+
+const loggerinfo = winston.createLogger({
+    level: 'info'
+  });
+  
+  const DailyRotateFile = require('winston-daily-rotate-file');
+  loggerinfo.configure({
+    level: 'info',
+    transports: [
+    new winston.transports.Console({
+        format: winston.format.combine(winston.format.colorize(), winston.format.simple(), winston.format.timestamp(), logFormatter),
+        }),
+      new DailyRotateFile({ filename: 'combined.log' })
+    ]
+  });
+
+
+const logger = (msg, type = "info")=>{
 
     const now = new Date().toLocaleString( 'sv', { timeZoneName: 'short' } )
-    switch (type) {
-        case "error":
-            console.error(`[ERROR-LOG]${now} : ${msg}`);
-            break;
-        case "log":
-        default:
-            console.error(`[INFO-LOG]${now} : ${msg}`);
-            break;
-    }
-}
+    loggerinfo.log({
+        level: type,
+        message: `[${type.toUpperCase()}-LOG]${now} : ${msg}`
+      });
+}   
 
 module.exports = logger 
